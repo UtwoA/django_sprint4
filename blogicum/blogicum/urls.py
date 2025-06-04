8512d67411673457030849e1f860f3176dbb10ad
Alexey <workspace.alexey@gmail.com>
2025-06-05 01:46:31 +0300

    w/o CBV

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,43 @@
+"""blogicum URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+from django.shortcuts import render
+from blogicum.views import profile_view, SignUpView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+def server_error(request):
+    return render(request, 'pages/500.html', status=500)
+
+urlpatterns = [
+    path('', include('blog.urls', namespace='blog')),
+    path('admin/', admin.site.urls),
+    path('pages/', include('pages.urls', namespace='pages')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/registration/', SignUpView.as_view(), name='registration'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

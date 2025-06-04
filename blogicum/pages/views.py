b80fab22ae8ef564a5d93b7f94823b887fcf6c9d
Alexey <workspace.alexey@gmail.com>
2025-06-05 01:53:06 +0300

    до тестов

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def about(request):
-    template = 'pages/about.html'
-    return render(request, template)
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
 
 
-def rules(request):
-    template = 'pages/rules.html'
-    return render(request, template)
-
+class RulesPageView(TemplateView):
+    template_name = 'pages/rules.html'
